bd2823b8f6f359b39fd5447e028e0d4fd103dcae
Polina Suboch <eonalion@gmail.com>
2017-03-13 20:51:33 +0300

    add auto-wiring

--- SpringCore/src/main/java/com/epam/suboch/app/App.java
+++ SpringCore/src/main/java/com/epam/suboch/app/App.java
@@ -4,43 +4,46 @@ import com.epam.suboch.client.Client;
 import com.epam.suboch.event.Event;
 import com.epam.suboch.event.EventType;
 import com.epam.suboch.logger.IEventLogger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
 /**
  *
  */
 public class App {
+    @Autowired
     private Client client;
+    @Autowired
+    @Qualifier("cacheFileEventLogger")
     private IEventLogger defaultLogger;
+    @Resource(name = "loggerMap")
     private Map<EventType, IEventLogger> loggerMap;
-
-
-    public App(Client client, Map<EventType, IEventLogger> loggerMap, IEventLogger defaultLogger) {
-        this.client = client;
-        this.loggerMap = loggerMap;
-        this.defaultLogger = defaultLogger;
-    }
 
     public void logEvent(EventType eventType, Event event) {
         IEventLogger eventLogger = loggerMap.get(eventType);
         if (eventLogger == null) {
-            eventLogger = defaultLogger;
+            eventLogger = defaultLogger; // qualified field
         }
         event.setMessage(eventType.toString());
         eventLogger.logEvent(event);
     }
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
         App app = (App) applicationContext.getBean("app");
 
         Event event = (Event) applicationContext.getBean("event");
         Event event2 = (Event) applicationContext.getBean("event");
         app.logEvent(EventType.INFO, event);
         app.logEvent(EventType.ERROR, event2);
+
+        System.out.println(app.client); // autowired
     }
 
 }
--- SpringCore/src/main/java/com/epam/suboch/client/Client.java
+++ SpringCore/src/main/java/com/epam/suboch/client/Client.java
@@ -6,11 +6,7 @@ package com.epam.suboch.client;
 public class Client {
     private int id;
     private String fullName;
-
-    public Client(int id, String fullName) {
-        this.id = id;
-        this.fullName = fullName;
-    }
+    private String greeting;
 
     public int getId() {
         return id;
@@ -26,5 +22,23 @@ public class Client {
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Client{");
+        sb.append("id=").append(id);
+        sb.append(", fullName='").append(fullName).append('\'');
+        sb.append(", greeting='").append(greeting).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
