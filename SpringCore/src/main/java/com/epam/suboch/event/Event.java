8ced61cb87c06f56b9a86512690f6ccce7ea1da3
Polina Suboch <eonalion@gmail.com>
2017-03-25 20:46:53 +0300

    replace old features with java 8 analogs

--- SpringCore/src/main/java/com/epam/suboch/event/Event.java
+++ SpringCore/src/main/java/com/epam/suboch/event/Event.java
@@ -1,29 +1,38 @@
 package com.epam.suboch.event;
 
-import java.text.DateFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.Random;
 
 /**
  *
  */
+
 public class Event {
     private int id;
     private String message;
-    private Date date;
-    private DateFormat dateFormat;
+    private LocalDateTime date;
+
     private static final int MAX_ID = 500;
     private static final int MIN_ID = 1;
 
-    public Event(Date date, DateFormat dateFormat) {
+    public Event(Date date) {
         Random random = new Random();
         this.id = random.nextInt(MAX_ID - MIN_ID + 1) - MIN_ID;
-        this.date = date;
-        this.dateFormat = dateFormat;
+        this.date = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public static boolean isDay() {
+        LocalDateTime d1 = LocalDateTime.now().withHour(7);
+        LocalDateTime d2 = LocalDateTime.now().withHour(22);
+        return LocalDateTime.now().isAfter(d1) && LocalDateTime.now().isBefore(d2);
     }
 
     @Override
@@ -31,7 +40,7 @@ public class Event {
         return "Event{" +
                 "id=" + id +
                 ", message='" + message +
-                ", date=" + dateFormat.format(date) +
+                ", date=" + date +
                 '}';
     }
 }
